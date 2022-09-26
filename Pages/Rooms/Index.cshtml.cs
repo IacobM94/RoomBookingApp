c5797935d8d3fa02a1db090f3f0ef7e7535bb60b
LewisBeane <Lewis.Beane@Oxfordshire.gov.uk>
2022-09-26 16:08:47 +0100

    Changes to Administrator Credentials

--- Pages/Rooms/Index.cshtml.cs
+++ Pages/Rooms/Index.cshtml.cs
@@ -11,7 +11,7 @@ using RoomBookingApp.Models;
 
 namespace RoomBookingApp.Pages.Rooms
 {
-    [Authorize(Policy = "RequireAdministratorRole")]
+    [Authorize(Roles = "Administrator")]
     public class IndexModel : PageModel
     {
         private readonly RoomBookingApp.Data.ApplicationDbContext _context;
--- Startup.cs
+++ Startup.cs
@@ -39,13 +39,6 @@ namespace RoomBookingApp
                     .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddControllersWithViews();
-
-
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy("RequireAdministratorRole",
-                     policy => policy.RequireRole("Administrator"));
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
